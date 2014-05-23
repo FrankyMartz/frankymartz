1c8b272d27c32bb6ec1ca8b5079d623c6140047d
Francisco Martinez <FrankyMartz@gmail.com>
2014-05-23 17:10:08 -0500

    Add (main.js) -> JS module aggregation file
    (main.js) serves as a bootstrap for all JavaScript modules to be
    processed with Browserify.

---
+++ _source/_assets/javascript/main.js
@@ -0,0 +1,14 @@
+'use strict';
+/**
+ * FrankyMartz.com JavaScript chunky goodness.
+ *
+ * @author Francisco Martinez <frankymartz@gmail.com>
+ * @version 1.0.0
+ * @copyright FrankyMartz 2014
+ */
+
+// Squash Browser Bugs
+require('./global/ie10-fix');
+
+
+

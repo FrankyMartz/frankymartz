729960fa51ae6d6c4333a13ec8a0b72d2e629c29
Francisco Martinez <FrankyMartz@gmail.com>
2014-09-22 12:51:19 -0500

    [Update] (preface.js) Rename, improve performance.
    Rename css class references from foreword to preface. Improve mobile
    performance for button to description transition.

--- _source/_assets/javascript/pages/preface.js
+++ _source/_assets/javascript/pages/preface.js
@@ -1,6 +1,6 @@
 'use strict';
 /** =========================================================================
- * Foreword
+ * Preface
  *
  * @author Franky Martinez <frankymartz@gmail.com>
  * @copyright FrankyMartz 2014
@@ -16,21 +16,31 @@ var secondToMillisecond = require('../util/cssSecondToMillisecond');
  * Toggle Classes for CSS3 image 3D flip
  **/
 function imageFlip() {
-  $('.foreword-fig__side2').toggleClass('js-foreword-fig-side1--flip');
-  $('.foreword-fig__side1').toggleClass('js-foreword-fig-side2--flip');
+  $('.preface-fig__side2').toggleClass('js-preface-fig-side1--flip');
+  $('.preface-fig__side1').toggleClass('js-preface-fig-side2--flip');
 }
 
-// Enable CSS3 image 3D flip on image(s) click
-$('.foreword-fig--flip').click(function(){
+
+/**
+ * Preface Button
+ */
+$('.preface-txt__button').click(function(){
+
+	$('.preface-txt__button:after').height('100%');
+
+	// Button Fade out
+	$(this).fadeTo(400,0);
 	imageFlip();
-});
-
-
-// Button
-$('.foreword-txt__button').click(function(){
-	$('.foreword-txt__button:after').height('100%');
-	$(this).fadeOut(400, function(){
-		$('.foreword-txt__description').delay(300).fadeIn(400);
+	$('.preface-txt__description').delay(700).queue(function(){
+		$(this).css('max-height', '43rem').dequeue();
 	});
-	imageFlip();
+	$(this).delay(800).queue(function(){
+			$(this).hide().dequeue();
+	});
+
+	// Enable CSS3 image 3D flip on image(s) click
+	$('.preface-fig--flip').click(function(){ imageFlip(); }).css('cursor', 'pointer');
+
+	return false;
 });
+

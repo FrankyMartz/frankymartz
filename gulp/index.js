4edaf45dfc30723ab48ac22599071a29ff5f92a1
Francisco Martinez <FrankyMartz@gmail.com>
2014-10-07 14:42:07 -0500

    [Update] (./gulp/index.js) improve error handling
    Add more arguments checks for (./gulp) package. Help reduce argument
    checks within tasks.

--- gulp/index.js
+++ gulp/index.js
@@ -18,16 +18,35 @@ module.exports = {
    * @param {String} name - User task to load in module:./gulp/task/
    * @param {Array} [dep] - Gulp Task dependencies
    * @param {*} opts - Configuration for task. (Refer to module docs).
-   * @throws {TypeError} Requires argument <name> typeof String
+   * @throws {TypeError} Require module <name> typeof String
+   * @throws {TypeError} Require <dep> typeof Array
+   * @throws {TypeError} Require <opts> typeof Object
    */
   addTask: function(name, dep, opts) {
+		//////////////////////////////
+		// Verify Requirements
+		//////////////////////////////
     if (!name || typeof name !== 'string') {
-      throw new Error('addTask: requires module <name> typeof String.');
+      throw new TypeError('addTask: Require module <name> typeof String.');
     }
+		if (dep && !Array.isArray(dep)) {
+			throw new TypeError('addTask: Require <dep> typeof Array.');
+		}
+		if (opts && typeof opts !== 'object') {
+			throw new TypeError('addTask: Require <opts> typeof Object.');
+		}
+
+		//////////////////////////////
+		// Defaults
+		//////////////////////////////
     if (!opts) {
       opts = dep;
       dep = undefined;
     }
+
+		//////////////////////////////
+		// Call Module
+		//////////////////////////////
     var path = './task/' + name;
     require(path)(name, dep, opts);
   }

c05965bb4664df6a1e8ccc08ef2583bcf165104c
Francisco Martinez <FrankyMartz@gmail.com>
2014-10-07 14:53:37 -0500

    [Update](browserify.js) Yep, Refactor
    Combine all Browserify Bundlers under one Gulp Task. Minimize function
    nesting. Improve argument checks. Fancy those LOG outputs.

--- gulp/task/browserify.js
+++ gulp/task/browserify.js
@@ -1,5 +1,5 @@
 'use strict';
-/**
+/*******************************************************************************
  * Browerify Gulp Task Module
  *
  * @author Franky Martinez <frankymartz@gmail.com>
@@ -10,108 +10,217 @@
  *    src:  './assets/js/\*.js', \\ escaped asterisk
  *    dest: './public/js/'
  * });
- */
-
-var glob = require('glob');
-var gulp = require('gulp');
+ *
+ * Reference [Browserify]{@link github.com/substack/node-browserify} for options.
+ * @typedef {Object} BrowserifyModuleOpts
+ * @property {(Array.<String>|String)} src - Glob(s) to expand
+ * @property {String} dest - Destination of output
+ *
+ ******************************************************************************/
 var path = require('path');
-var gutil = require('gulp-util');
+
+// THIRD PARTY MODULE
+var gulp = require('gulp');
 var browserify = require('browserify');
 var watchify = require('watchify');
-var fromArgs = require('watchify/bin/args');
-//var errorHandler = require('../util/errorHandler');
+var gUtil = require('gulp-util');
 var source = require('vinyl-source-stream');
-var streamify = require('gulp-streamify');
-var rev = require('gulp-rev');
-var gzip = require('gulp-gzip');
+var glob = require('glob');
+var prettyMS = require('pretty-ms');
+
+// THIRD PARTY MODULE FUNCTION
+var gLog = gUtil.log;
+var gHue = gUtil.colors;
+
+// GLOBALS VARIABLE
+var PLUGIN_NAME = 'browserify';
+var PRODUCTION = global.isProduction;
+
+// CUSTOM MODULE
+var errorHandler = require('../util/errorHandler');
+var gPluginError = gUtil.PluginError.bind(null, PLUGIN_NAME);
+
+
 
 /**
- * Reference [Browserify]{@link github.com/substack/node-browserify} for API
- * configuration options.
- * @summary module:gulp/browserify Configuration Object
- * @typedef {Object} BrowserifyModuleOpts
- * @property {Array.<String>} src - File or Glob to process
- * @property {String} dest - Destination of output
- * @property {Object} [gzip] -Reference [gulp-gzip]{@link github.com/jstuckey/gulp-gzip} documentation
+ * Flatten Multidimensional Arrays
+ * @kind Function
+ * @param {Array} arg - Array to Flatten
+ * @throws {Error} Require <arg> type of Array
+ * @return Array - Flattened Array.
  */
+function flattenArray(arg){
+	if (!arg || !Array.isArray(arg)) {
+	  throw new Error('flattenArray: <arg> typeof (Array) required');
+	}
+	var temp = [];
+	function inception(list) {
+		var len = list.length;
+		var i;
+	  for (i = 0; i < len; i++) {
+		  if (Array.isArray(list[i])) {
+			    inception(list[i]);
+			} else {
+			    temp.push(list[i]);
+			}
+		}
+		return temp;
+	}
+	return inception(arg);
+}
+
+
+
+/**
+ * Browserify Bundle Finish Log Message
+ * @kind Function
+ * @param {String} fileName - Current file in process
+ * @param {Number} startTime - time in ms since file processing start
+ *
+ */
+function logBundleFinish(fileName, startTime) {
+	var now = Date.now();
+	var prettyTime = prettyMS(now - startTime);
+	gLog(
+		gHue.yellow.bgBlue('Browserify:'), 'Updated',
+		gHue.yellow('"' + fileName + '"'), 'in',
+		gHue.magenta(prettyTime)
+	);
+}
+
+
+
+/**
+ * Enable Browserify|Watchify on file
+ * @kind Function
+ * @param {String} src - file to Browserify and/or Watchify
+ * @param {String} dest - Output destination
+ * @param {Function} - Function to run on completion
+ * @return {Object} - Browserify Object Bundler
+ */
+function browserifyFile(src, dest, cb) {
+	if (!src || !dest) {
+		throw new Error('browserifyFile: <src> and <dest> required');
+	}
+	if (typeof src !== 'string') {
+		throw new TypeError('browserifyFile: <src> typeof (String) required');
+	}
+	if (typeof dest !== 'string') {
+		throw new TypeError('browserifyFile: <dest> typeof (String) required');
+	}
+	if (cb && (Object.prototype.toString.call(cb) !== '[object Function]')) {
+		throw new TypeError('browserifyFile: <cb> typeof (Function) required');
+	}
+	cb = cb || function(){return;};
+	var bundler = browserify({
+		entries: [src],
+		debug: (PRODUCTION ? false : true),
+		// Watchify Required Properties
+		cache: {},
+		packageCache: {},
+		fullPaths: true
+	});
+
+	function bundle() {
+		var fileName = path.basename(src);
+		var then = Date.now();
+		return bundler
+			.bundle()
+			.pipe(source(fileName))
+			.on('error', errorHandler)
+			.pipe(gulp.dest(dest))
+			.on('end', function(){
+				logBundleFinish(fileName, then);
+				cb();
+			});
+	}
+
+	if (!PRODUCTION) {
+	  bundler = watchify(bundler);
+		bundler.on('update', bundle);
+	}
+	return bundle();
+}
+
+
+
+/**
+ * Browserify Each Item in Array
+ * @kind Function
+ * @param {Array} files - List of files to Browserify
+ * @param {String} - Output destination
+ * @param {Function} - Function to run on completion
+ */
+function browserifyArray(files, dest, cb) {
+	// Validate Arguments
+	if (!files || !Array.isArray(files)) {
+	  throw new TypeError('browserifyArray: <files> typeof (Array) required');
+	}
+	if (!dest | typeof dest !== 'string') {
+		throw new TypeError('browserifyArray: <dest> typeof (String) required');
+	}
+	if (cb && (Object.prototype.toString.call(cb) !== '[object Function]')) {
+		throw new TypeError('browserifyArray: <cb> typeof (Function) required');
+	}
+	cb = cb || function(){return;};
+
+	files.forEach(function(file, index) {
+		browserifyFile(file, dest, function(){
+			if (index <= 0) {
+				cb();
+			}
+		});
+	});
+}
+
+
 
 /**
  * Module must be loaded through Package Loader (index.js)
+ * @kind Function
  * @param {String} name - String for Gulp Task reference
  * @param {Array.<String>} [dep] - Gulp Task Dependencies
  * @param {BrowserifyModuleOpts} args - Configuration options for task
- * @throws {Error} Require argument <args> typeof Object
- * @throws {Error} Require <args>.src typeof Array
- * @throws {Error} Require <args>.dest typeof String
+ * @throws {PluginError} Require <args> typeof Object
+ * @throws {PluginError} Require <args>.src typeof Array
+ * @throws {PluginError} Require <args>.dest typeof String
  */
-module.exports = function(name, dep, args) {
-  // Throw Errors
-  if (args || typeof args !== 'object') {
-    if (!args.src || !Array.isArray(args.src)) {
-      throw new Error('<args>.src typeof Array.String is required.');
-    }
-    if (!args.dest || typeof args.dest !== 'string') {
-      throw new Error('<args>.dest typeof String is required.');
-    }
-  }
-  else {
-    throw new Error('Module requires argument <args> typeof Object.');
-  }
+function plugin(name, deps, args){
+	//////////////////////////////
+	// Verify Requirements
+	//////////////////////////////
+	if (!args || (typeof args !== 'object')) {
+		throw new gPluginError('<args> typeof (Object) required');
+	}
+	if (!args.src || !(Array.isArray(args.src) || (typeof args.src === 'string'))) {
+		throw new gPluginError('<args>.src typeof (Array.String) or (String) required');
+	}
+	if (!args.dest || typeof args.dest !== 'string') {
+		throw new gPluginError('<args>.dest typeof (String) required');
+	}
 
-  // Convert globs to iterable array
-  var depends = [].concat.apply([],
-    args.src.map(function(element){
-      return glob.sync(element);
-    })
-  );
+	//////////////////////////////
+	// Task
+	//////////////////////////////
+	gulp.task(name, deps, function(cb) {
+		var src = args.src;
 
-  function browserifyFile(element) {
-    gulp.task(element, dep, function() {
-			args.gzip = args.gzip || { append: false, gzipOptions: { level: 9 } };
-
-			var bundler = browserify({
-				entries: [element],
-				debug: true,
-				// Watchify Required Properties
-				cache: {},
-				packageCache: {},
-				fullPaths: true
+		if (Array.isArray(src)) {
+			src = src.map(function(item){
+				return glob.sync(item);
 			});
 
-      function bundle() {
-				var filename = path.basename(element);
-				var startTime = process.hrtime();
+		} else {
+			src = glob.sync(src);
+		}
 
-        return bundler
-					.bundle()
-					.on('error', gutil.log.bind(gutil, 'Browserify Error'))
-          .pipe(source(filename))
-          .pipe(streamify(gutil.env.type === 'production' ? rev() : gutil.noop()))
-					.pipe(streamify(gutil.env.type === 'production' ? gzip(args.gzip) : gutil.noop()))
-          .pipe(gulp.dest(args.dest))
-					.on('end', function(){
-						var diff = process.hrtime(startTime);
-						var prettyTime = diff[0] + (diff[1] / 1e6);
-						prettyTime = prettyTime.toFixed(2);
-						gutil.log(
-							gutil.colors.blue('Browserify:'), 'Updated',
-							gutil.colors.yellow("'" + filename + "'"), 'in',
-							gutil.colors.magenta(prettyTime + 'ms'));
-					});
+		src = flattenArray(src);
 
-      }
+		// cb -> Tell Gulp the Task is Complete.
+		browserifyArray(src, args.dest, cb);
 
-			// Disable Watch on 'production' build
-			if (gutil.env.type !== 'production') {
-				bundler = watchify(bundler);
-				bundler.on('update', bundle);
-			}
+	});
+}
 
-      return bundle();
-    });
-  }
 
-  depends.forEach(browserifyFile);
-  gulp.task(name, depends);
-};
-
+module.exports = plugin;

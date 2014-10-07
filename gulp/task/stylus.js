6ebc7ffd477e9c3697cd1a9a521a58e4dc10d73d
Francisco Martinez <FrankyMartz@gmail.com>
2014-10-07 15:01:40 -0500

    [Update](stylus.js) KISS. Remove optimizations.
    Remove optimizations from (./gulp/task/stylus.js). Concatenate those
    into separate task.

--- gulp/task/stylus.js
+++ gulp/task/stylus.js
@@ -1,5 +1,5 @@
 'use strict';
-/**
+/*******************************************************************************
  * Stylus Gulp Task Module
  *
  * @author Franky Martinez <frankymartz@gmail.com>
@@ -10,82 +10,73 @@
  *   src: './assets/stylus/\*.styl',  \\ escaped asterisk
  *   dest: './public/css'
  * });
- */
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var stylus = require('gulp-stylus');
-var prefix = require('gulp-autoprefixer');
-var minifyCSS = require('gulp-minify-css');
-var rev = require('gulp-rev');
-var gzip = require('gulp-gzip');
-var errorHandler = require('../util/errorHandler');
-var watch = require('gulp-watch');
-/**
+ *
  * Reference package documentation for available API configuration options.
- * @summary module:gulp/stylus Configuration Object.
  * @typedef {Object} StylModuleOpts
  * @property {(String|Array.<String>)} src - File or Glob to process
  * @property {String} dest - Destination of output
- * @property {String} watch - Path to observe
  * @property {Object} [stylus] - Reference [gulp-stylus]{@link github.com/stevelacy/gulp-stylus} documentation
  * @property {Array.<String>} [autoprefixer] - Reference [gulp-autoprefixer]{@link github.com/Metrime/gulp-autoprefixer} documentation
- * @property {Object} [minifycss] - Reference [gulp-minify-css]{@link github.com/jonathanepollack/gulp-minify-css} documentation
- * @property {Object} [gzip] -Reference [gulp-gzip]{@link github.com/jstuckey/gulp-gzip} documentation
- */
+ ******************************************************************************/
+// THIRD PARTY MODULE
+var gulp = require('gulp');
+var gUtil = require('gulp-util');
+var gStylus = require('gulp-stylus');
+var gAutoPrefix = require('gulp-autoprefixer');
+var gWatch = require('gulp-watch');
+
+// GLOBALS VARIABLE
+var PLUGIN_NAME = 'stylus';
+var PRODUCTION = global.IS_PRODUCTION;
+
+// CUSTOM MODULE
+var errorHandler = require('../util/errorHandler');
+var gPluginError = gUtil.PluginError.bind(null, PLUGIN_NAME);
 
 /**
  * Module must be loaded through Package Loader (index.js)
  * @param {String} name - String for Gulp Task reference
  * @param {Array.<String>} [dep] - Gulp Task Dependencies
  * @param {StylModuleOpts} args - Configuration options for task
- * @throws {Error} Require argument <args> typeof Object
- * @throws {Error} Require arguments <args>.src && <args>.dest
- * @throws {TypeError} <args>.stylus must be typeof Object
- * @throws {TypeError} <args>.autoprefixer must be typeof Array
- * @throws {TypeError} <args>.minifycss must be typeof Object
+ * @throws {PluginError} Require argument <args> typeof Object
+ * @throws {PluginError} Require arguments <args>.src && <args>.dest
+ * @throws {PluginError} <args>.stylus must be typeof Object
+ * @throws {PluginError} <args>.autoprefixer must be typeof Array
  */
 module.exports = function(name, dep, args) {
-  // Throw Errors
-  if (args || typeof args === 'object') {
-    if (!args.src || !args.dest) {
-      throw new Error('Arguments <args>.src && <args>.dest are required.');
-    }
-    if (args.stylus && typeof args.stylus !== 'object') {
-      throw new TypeError('<args>.stylus argument must be typeof Object.');
-    }
-    if (args.autoprefixer && !Array.isArray(args.autoprefixer)) {
-      throw new TypeError('<args>.autoprefixer argument must be typeof Array.');
-    }
-    if (args.minifycss && typeof args.minifycss !== 'object') {
-      throw new TypeError('<args>.minifycss argument must be type Object');
-    }
-  }
-  else {
-    throw new Error('Stylus module requires <args> argument typeof Object');
-  }
-  // Set Defaults
+	//////////////////////////////
+	// Verify Requirements
+	//////////////////////////////
+	if (!args || typeof args !== 'object') {
+		throw new gPluginError('<args> typeof Object required. Refer to documentation');
+	}
+	if (!args.src || !args.dest) {
+		throw new gPluginError('<args>.src && <args>.dest required.');
+	}
+	if (args.stylus && typeof args.stylus !== 'object') {
+		throw new gPluginError('<args>.stylus must be typeof Object');
+	}
+	if (args.autoprefixer && !Array.isArray(args.autoprefixer)) {
+		throw new gPluginError('<args>.autoprefixer must be typeof Array');
+	}
+
+	//////////////////////////////
+	// Defaults
+	//////////////////////////////
   args.stylus = args.stylus || {};
   args.stylus.errors = true;
   args.autoprefixer = args.autoprefixer || ['last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'];
-  args.minifycss = args.minifycss || {};
-	args.gzip = args.gzip || { append: false, gzipOptions: { level: 9 } };
-  // Build Gulp Task
+
+	//////////////////////////////
+	// Task
+	//////////////////////////////
   gulp.task(name, dep, function() {
-		gulp.src(args.src)
-      .pipe(stylus(args.stylus))
+		return gulp.src(args.src)
+			.pipe(PRODUCTION ? gUtil.noop() : gWatch(args.watch))
+      .pipe(gStylus(args.stylus))
       .on('error', errorHandler)
-      .pipe(prefix(args.autoprefixer, {cascade: true}))
-      .pipe(gutil.env.type === 'production' ? minifyCSS(args.minifycss) : gutil.noop())
-      .pipe(gutil.env.type === 'production' ? rev() : gutil.noop())
-      .pipe(gutil.env.type === 'production' ? gzip(args.gzip) : gutil.noop())
+      .pipe(gAutoPrefix(args.autoprefixer, {cascade: true}))
       .pipe(gulp.dest(args.dest));
   });
 
-
-	// Disable Watch on 'production' build
-	if (gutil.env.type !== 'production') {
-		watch(args.watch, function(){
-			gulp.start(name);
-		});
-	}
 };
